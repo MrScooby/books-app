35da370983f16b9dbd23dee0c758ef037f93df1c
MrScooby <maciej.kubiczek@gmail.com>
2026-04-02 23:45:29 +0200

    disable adding new book

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,5 +1,14 @@
-import AddBookForm from '@/components/add/AddBookForm'
+import { Alert, Typography, Box } from '@mui/material'
 
 export default function AddPage() {
-  return <AddBookForm />
+  return (
+    <Box sx={{ maxWidth: 500 }}>
+      <Typography variant="h5" fontWeight={600} sx={{ mb: 2 }}>
+        Add Book
+      </Typography>
+      <Alert severity="info">
+        Adding new books is temporarily disabled.
+      </Alert>
+    </Box>
+  )
 }
